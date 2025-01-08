a2ace2a415f5d654e625908a11e09124fba2d40d
Aidan-Killeen <killeeai@tcd.ie>
2025-01-08 15:45:12 +0000

    Improved the launcer codes readability, and made it end the launcher program after it initialises the selected program

--- Launcher.py
+++ Launcher.py
@@ -1,31 +1,37 @@
 import os
+from typing import List
+import subprocess
 
 #Finding Program names - each one is named after the directory it is in
-MYDIR = "."
-folder_names = []
-for entry_name in os.listdir(MYDIR):
-    entry_path = os.path.join(MYDIR, entry_name)
-    if os.path.isdir(entry_path) and entry_name != ".git":
-        folder_names.append(entry_name)
+def list_subfolders(dir: str) -> List[str]:
+    # dir = "."
+    folder_names = []
+    for entry_name in os.listdir(dir):
+        entry_path = os.path.join(dir, entry_name)
+        if os.path.isdir(entry_path) and entry_name != ".git":
+            folder_names.append(entry_name)
+    return folder_names
 
-# print(folder_names)
+def menu(folder_names: List[str]) -> int:
+    index = 0
+    while(True):
+        print("Available Programs:")
+        for i in range(len(folder_names)):
+            print("\t", i+1, folder_names[i])
+        selection = input("Select the program to launch: ")
+        if selection.isdigit() and int(selection) in range(1, len(folder_names)+1):
+            index = int(selection) - 1
+            break
+        else:
+            print("Invalid Input: Enter a number from 1 to", len(folder_names))
+    return index
 
-#Menu
-while(True):
-    print("Available Programs:")
-    for i in range(len(folder_names)):
-        print("\t", i+1, folder_names[i])
-    selection = input("Select the program to launch: ")
-    if selection.isdigit() and int(selection) in range(1, len(folder_names)+1):
-        selection = int(selection) - 1
-        break
-    else:
-        print("Invalid Input: Enter a number from 1 to", len(folder_names))
-    
-file = folder_names[selection]
-path = "./" + file + "/" + file + ".py"
-print("Running", file)
-
-#Running the selected File
-import subprocess
-subprocess.run(["python", path])
+if __name__=="__main__":
+    folder_names = list_subfolders(".")
+    index = menu(folder_names)
+        
+    file = folder_names[index]
+    path = "./" + file + "/" + file + ".py"
+    print("Running", file)
+    subprocess.Popen(["python", path])
+    print("End")
