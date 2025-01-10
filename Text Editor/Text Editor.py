3434b6fcc4df0f32d59949e5ea56d89896a6a885
Aidan-Killeen <killeeai@tcd.ie>
2025-01-10 15:22:01 +0000

    Managed to implement opening a specified file, and saving changes to a file

--- Text Editor/Text Editor.py
+++ Text Editor/Text Editor.py
@@ -1,47 +1,51 @@
 from tkinter import *
 
-def load_file(filename: str):
-    with open("./Text Editor/"+filename, 'r') as file:
+def load_file():
+    global filename
+    filename = "Testfile.txt"
+    root.title(filename)
+    with open(path+filename, 'r') as file:
         data = file.read()
         file_text.insert(END, data)
 
-def save():
+def new_file():
+    return
+
+def save_file():
     INPUT = file_text.get("1.0", "end-1c")
     print(INPUT)
+    global filename
+    if(filename == ""):
+        return #Run new_file command
+    with open(path+filename, 'w') as file:
+        file.write(INPUT)
+        file.close()  
 
 
 
 root = Tk()
 root.geometry("500x500")
 root.title("unknown file")
-
+path = "./Text Editor/"
+filename = ""
 
 file_text = Text(root, width = 50)
 file_text.configure(font = ("Arial", 20, ""))
 
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=new_file)
+filemenu.add_command(label="Open", command=load_file)
+filemenu.add_command(label="Save", command=save_file)
+filemenu.add_separator()
+filemenu.add_command(label = "Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
 
-
-
+root.config(menu=menubar)
 #input_box.grid(row=0, column = 0)
 
-
-
-
-save_button = Button(root, height = 2,
-                 width = 20, 
-                 text ="Save",
-                 command = lambda:save())
-
-
-
-save_button.pack(expand = YES, fill = BOTH)
 file_text.pack(expand = YES, fill = BOTH)
 
-import os
-print(os.listdir('.'))
-
-filename = "Testfile.txt"
-load_file(filename)
 
 root.mainloop()
 
