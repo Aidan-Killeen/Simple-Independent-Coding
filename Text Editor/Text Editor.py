f89301c9a50019c6876b8d73cd5a805f047bd4e4
Aidan-Killeen <killeeai@tcd.ie>
2025-01-13 18:04:13 +0000

    Improved text editor with libraries - can now browse file system. Need to add error checking

--- Text Editor/Text Editor.py
+++ Text Editor/Text Editor.py
@@ -1,25 +1,38 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
 def load_file():
-    global filename
-    filename = "Testfile.txt"
-    root.title(filename)
-    with open(path+filename, 'r') as file:
+    global open_file
+    file_path = askopenfilename()
+
+    with open(file_path, 'r') as file:
+    #with open(path+filename, 'r') as file:    
         data = file.read()
+        file_text.delete('1.0', END)
         file_text.insert(END, data)
+        open_file = file_path
+        root.title(file_path)
+    
 
 def new_file():
-    return
+    file_text.delete('1.0', END)
+    global open_file
+    open_file = ""
+    save_file()
+
 
 def save_file():
     INPUT = file_text.get("1.0", "end-1c")
     print(INPUT)
-    global filename
-    if(filename == ""):
-        return #Run new_file command
-    with open(path+filename, 'w') as file:
+    global open_file
+    if(open_file == ""):
+        open_file = asksaveasfilename()
+    with open(open_file, 'w') as file:
         file.write(INPUT)
-        file.close()  
+        file.close()
+    root.title(open_file)
+
 
 
 
@@ -27,7 +40,7 @@ root = Tk()
 root.geometry("500x500")
 root.title("unknown file")
 path = "./Text Editor/"
-filename = ""
+open_file = ""
 
 file_text = Text(root, width = 50)
 file_text.configure(font = ("Arial", 20, ""))
