1a79c311340b77bb3be758f611e1e6180e4c6c8d
Aidan-Killeen <killeeai@tcd.ie>
2025-01-14 15:26:17 +0000

    Added Exception handling

--- Text Editor/Text Editor.py
+++ Text Editor/Text Editor.py
@@ -6,59 +6,71 @@ def load_file():
     global open_file
     file_path = askopenfilename()
 
-    with open(file_path, 'r') as file:
-    #with open(path+filename, 'r') as file:    
-        data = file.read()
-        file_text.delete('1.0', END)
-        file_text.insert(END, data)
-        open_file = file_path
-        root.title(file_path)
+    try:
+        with open(file_path, 'r') as file:
+        #with open(path+filename, 'r') as file:    
+            data = file.read()
+            file_text.delete('1.0', END)
+            file_text.insert(END, data)
+            open_file = file_path
+            root.title(file_path)
+    except FileNotFoundError:
+        return
     
 
 def new_file():
+    temp = file_text.get("1.0", "end-1c")
     file_text.delete('1.0', END)
     global open_file
+    prev_file = open_file
     open_file = ""
-    save_file()
+    if not save_file():
+        open_file = prev_file
+        file_text.insert(END, temp)
 
 
 def save_file():
     INPUT = file_text.get("1.0", "end-1c")
     print(INPUT)
     global open_file
-    if(open_file == ""):
-        open_file = asksaveasfilename()
-    with open(open_file, 'w') as file:
-        file.write(INPUT)
-        file.close()
-    root.title(open_file)
+    try:
+        if(open_file == ""):
+            open_file = asksaveasfilename()
+        with open(open_file, 'w') as file:
+            file.write(INPUT)
+            file.close()
+        root.title(open_file)
+        return True
+    except FileNotFoundError:
+        return False
+        
 
 
 
+if __name__=="__main__":
+    root = Tk()
+    root.geometry("500x500")
+    root.title("unknown file")
+    path = "./Text Editor/"
+    open_file = ""
 
-root = Tk()
-root.geometry("500x500")
-root.title("unknown file")
-path = "./Text Editor/"
-open_file = ""
+    file_text = Text(root, width = 50)
+    file_text.configure(font = ("Arial", 20, ""))
 
-file_text = Text(root, width = 50)
-file_text.configure(font = ("Arial", 20, ""))
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=new_file)
+    filemenu.add_command(label="Open", command=load_file)
+    filemenu.add_command(label="Save", command=save_file)
+    filemenu.add_separator()
+    filemenu.add_command(label = "Exit", command=root.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
 
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=new_file)
-filemenu.add_command(label="Open", command=load_file)
-filemenu.add_command(label="Save", command=save_file)
-filemenu.add_separator()
-filemenu.add_command(label = "Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
+    root.config(menu=menubar)
+    #input_box.grid(row=0, column = 0)
 
-root.config(menu=menubar)
-#input_box.grid(row=0, column = 0)
-
-file_text.pack(expand = YES, fill = BOTH)
+    file_text.pack(expand = YES, fill = BOTH)
 
 
-root.mainloop()
+    root.mainloop()
 
