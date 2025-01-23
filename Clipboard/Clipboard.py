ed169ddbf8fa81f294052b14f2fa6f651a06fc52
Aidan-Killeen <killeeai@tcd.ie>
2025-01-23 15:48:31 +0000

    Added prompt to make it clearer when a new button is being requested. Program can now safely exit without launching the macro

--- Clipboard/Clipboard.py
+++ Clipboard/Clipboard.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Button, Entry
+from tkinter import Tk, Button, Entry, Label, Toplevel, messagebox
+from tkinter.simpledialog import Dialog
 import pyperclip
 
 import re
@@ -22,6 +23,22 @@ def adjust(txt):
 
 changing_shortcut = False
 listening = False
+top = None
+def change():
+    #Needs to create a popup
+    global changing_shortcut
+    changing_shortcut = True
+    global top
+    top = Toplevel(root)
+    top.geometry("300x100")
+    top.title("Select Key")
+    Label(top, text= "Press any Key").grid(column=0, row=0)
+
+    top.columnconfigure(0, weight=1)
+    top.rowconfigure(0, weight=1)
+    #top = messagebox.showinfo(title="Select Key")
+    #top = Dialog(root, title="Select Key")
+
 def get_shortcut(key):
     global changing_shortcut
     global shortcut
@@ -30,6 +47,9 @@ def get_shortcut(key):
         short_disp.delete(0, "end")
         short_disp.insert( len(short_disp.get()), '{}'.format(shortcut))
         changing_shortcut = False
+        global top
+        if top != None:
+            top.destroy()
     elif listening:
         print('{} pressed'.format(key))
         if key == shortcut:
@@ -37,17 +57,17 @@ def get_shortcut(key):
         elif key == Key.esc:
             return False
 
-def change():
-    #Needs to create a popup
-    global changing_shortcut
-    changing_shortcut = True
+
 
 def macro():
     root.destroy()
     global listening
     listening = True
     
-    
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+        listener.stop()
 
 shortcut = Key.insert
 
@@ -74,6 +94,7 @@ if __name__=="__main__":
     min = Button(root, text="Launch Macro", command=macro)
     min.grid(row=2)
 
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
     print("Macro launched")
     listener.join()
