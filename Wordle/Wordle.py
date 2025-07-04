9673fa6945b9e97c477de2b0e85bb7ababe0540c
Aidan-Killeen <killeeai@tcd.ie>
2025-07-04 16:09:26 +0100

    Global variables now stored on cookies

--- Wordle/Wordle.py
+++ Wordle/Wordle.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
+import json
 
 app = Flask(__name__)
 
@@ -24,35 +25,49 @@ def make_guess(guess: str, target: str):
 # Options - retrieve json using random date from 19/06/2021 https://www.nytimes.com/svc/wordle/v2/{yyyy}-{MM}-{dd}.json
 
 target = "minow"
-guess_no = 0
+#guess_no = 0
+#prev_guesses = []
 
 @app.route("/")
 def game():
-    global guess_no
+    #global guess_no
+    guess_no = int(request.cookies.get('guess_no', 0))
+    prev_guesses = json.loads(str(request.cookies.get('prev_guesses', '{ "prior":[]}')))
+    
+    print(prev_guesses)
+    valid = False
     guess = request.args.get("guess", "")
     if guess:
         if len(guess) != len(target):
             output = "Error: Length mismatch"
         else:
             out, solved = make_guess(guess, target)
+            valid = True
             if not solved:
                 output = "Incorrect guess, try again " + str(out)
             else:
                 output = "Correct!"
+            prev_guesses["prior"].append((guess, out))
             guess_no += 1
     else:
         output = ""
 
-    return(
-        """<form action="" method="get">
+    resp = make_response("""<form action="" method="get">
                 Make a Guess: <input type="text" name="guess">
                 <input type="submit" value="Submit">
             </form>"""
         + "Last Guess: "
         + output
+        + ", Prior Guesses: "
+        + str(prev_guesses)
         + "<br>Current Guess: "
-        + str(guess_no)
-    )
+        + str(guess_no))
+    
+    resp.set_cookie('guess_no', str(guess_no))
+    if valid:
+        print(prev_guesses)
+        resp.set_cookie('prev_guesses', json.dumps(prev_guesses))
+    return(resp)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
